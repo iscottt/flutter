7e99f1f776262059d9a8c7bcc28fd767bb6253a3
Scott <1194379729@qq.com>
2022-11-30 17:31:06 +0800

    docs: 📝Dart变量示例

---
+++ Dart/chapter_2/variable.dart
@@ -0,0 +1,23 @@
+
+void main() {
+  var name = '张三';
+  var age = 20;
+  var height = 1.88;
+  var flag = true;
+  print(name); // 张三
+  print(age); // 20
+  print(height); // 1.88
+  print(flag); // true
+  test();
+}
+
+void test() {
+  String name = '张三';
+  int age = 20;
+  double height = 1.88;
+  bool flag = true;
+  print(name); // 张三
+  print(age); // 20
+  print(height); // 1.88
+  print(flag); // true
+}
